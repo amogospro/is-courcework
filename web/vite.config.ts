e6b48794ec2ac4e580e8567259ecd4b3aa3eafd3
Wgmlgz <62064707+Wgmlgz@users.noreply.github.com>
2024-12-13 01:12:15 +0300

    maybe js is as bad as java

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,10 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
 import UnoCSS from 'unocss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import topLevelAwait from 'vite-plugin-top-level-await';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
-import { type Connect, type Plugin } from 'vite';
+import { defineConfig, type Connect, type Plugin } from 'vite';
+// import wasm from "vite-plugin-wasm";
 
 // const crossOriginIsolationMiddleware: Connect.NextHandleFunction = (request, response, next) => {
 //   if (request.url === '/service/administration/monitoring') {
@@ -34,15 +34,21 @@ import { type Connect, type Plugin } from 'vite';
 
 export default defineConfig({
   plugins: [
+    // wasm(),
+    topLevelAwait(),
+
+    // nodePolyfills(),
     sveltekit(),
     UnoCSS(),
     // nodePolyfills(),
     // tsconfigPaths(),
-    topLevelAwait(),
     // crossOriginIsolation()
   ],
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}']
+  },
+  server: {
+    cors: true
   },
   resolve: {
     alias: {
